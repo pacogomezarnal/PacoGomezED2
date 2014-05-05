52d2a318a0de863bf5f1b13f75f8bedc775a0c19
pacogomezedutic <edutic.es@gmail.com>
2014-05-05 15:15:20 +0200

    Modificando

--- GitProject/src/GitProject.java
+++ GitProject/src/GitProject.java
@@ -4,7 +4,7 @@ public class GitProject {
 	public static void main(String[] args) {
 		System.out.println("Hola compañeros. Soy Paco");
 		
-		System.out.println("Otra linea de codigo");
+		System.out.println("Otra linea de codigo 2");
 
 	}
 
