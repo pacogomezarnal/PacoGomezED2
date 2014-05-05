46913286ba17b185196c74332cef9726b6952813
pacogomezedutic <edutic.es@gmail.com>
2014-05-05 15:14:21 +0200

    Modificada rama prueba

--- GitProject/src/GitProject.java
+++ GitProject/src/GitProject.java
@@ -5,6 +5,8 @@ public class GitProject {
 		System.out.println("Hola compañeros. Soy Paco");
 		
 		System.out.println("ESTA ES LA RAMA PRUEBA");
+		
+		System.out.println("ESTA ES LA RAMA PRUEBA 2");
 
 	}
 
