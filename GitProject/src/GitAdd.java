2cc82506672f1d2f6713fb736998e5e099cd4d1a
pacogomezarnal <edutic.es@gmail.com>
2014-05-16 15:00:12 +0200

    Signed-off-by: pacogomezarnal <edutic.es@gmail.com>

--- GitProject/src/GitAdd.java
+++ GitProject/src/GitAdd.java
@@ -3,6 +3,7 @@ public class GitAdd {
 
 	String nombre="";
 	int a=0;
+	int b=0;
 	
 	public GitAdd() {
 		// TODO Auto-generated constructor stub
--- GitProject/src/GitProject.java
+++ GitProject/src/GitProject.java
@@ -2,7 +2,7 @@
 public class GitProject {
 
 	public static void main(String[] args) {
-		System.out.println("Hola compañeros. Soy Paco");
+		System.out.println("Hola compañeros. Soy Paca");
 		
 		System.out.println("Otra linea de codigo 2");
 
