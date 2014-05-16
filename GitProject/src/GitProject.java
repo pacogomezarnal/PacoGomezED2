d03428e676ee24c53a9a69e33a314702e9b966e6
pacogomezarnal <edutic.es@gmail.com>
2014-05-16 14:53:30 +0200

    Signed-off-by: pacogomezarnal <edutic.es@gmail.com>

--- GitProject/src/GitProject.java
+++ GitProject/src/GitProject.java
@@ -1,13 +1,14 @@
 
 public class GitProject {
 
+	private static  int a=0;
 	public static void main(String[] args) {
 		System.out.println("Hola compañeros. Soy Paco");
 		
 		System.out.println("ESTA ES LA RAMA PRUEBA");
 		
 		System.out.println("ESTA ES LA RAMA PRUEBA 2");
-
+		a=2;
 	}
 
 }
